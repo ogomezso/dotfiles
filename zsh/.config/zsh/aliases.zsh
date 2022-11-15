7b67007fc8580fb12a20f41d479aabde996ed01e
Juanlu Sanz <juanljr2@gmail.com>
2022-11-15 21:25:32 +0100

    Added ZSH

---
+++ zsh/.config/zsh/aliases.zsh
@@ -0,0 +1,7 @@
+# Alias
+
+# ALIAS COMMANDS
+alias ls="exa --icons --group-directories-first"
+alias ll="exa --icons --group-directories-first -l"
+
+alias grep='grep --color'
---
+++ zsh/.zshrc
@@ -0,0 +1,4 @@
+for config_file ($HOME/.config/zsh/*.zsh) source $config_file
+
+# Load Starship
+eval "$(starship init zsh)"
